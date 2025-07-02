8769b1fe2ddd8d6e4cc9b5fde5197607d853a6f4
Schwopo <lofskim@gmail.com>
2025-07-02 14:42:51 +0200

    Need one test

--- schwopo-app/src/App.test.js
+++ schwopo-app/src/App.test.js
@@ -2,7 +2,11 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 //test('renders learn react link', () => {
- // render(<App />);
+  //render(<App />);
   //const linkElement = screen.getByText(/learn react/i);
   //expect(linkElement).toBeInTheDocument();
 //});
+
+test('Need one test xd', () => {
+  expect(1).toBe(1);
+});
